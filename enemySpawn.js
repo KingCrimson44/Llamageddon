b9a56e680d38616d45392338764e6daae296d04d
KingCrimson44 <jdjohnson3@email.neit.edu>
2014-07-04 13:08:23 -0400

    Revert "Revert "Llamageddon scripts""
    This reverts commit 6fc5b6ed2c63d0d651c534c4ca6250a63497cce3.

---
+++ enemySpawn.js
@@ -0,0 +1,50 @@
+﻿#pragma strict
+
+var spawnLeft : Transform;
+var spawnRight : Transform;
+
+var llamaLeft : GameObject;
+var llamaRight: GameObject;
+
+var canSpawnRight: boolean;
+var canSpawnLeft: boolean;
+
+var spawnTimeX : int;
+var spawnTimeY : int;
+
+function Start () {
+canSpawnRight = true;
+canSpawnLeft = true;
+spawnTimeX = 2;
+spawnTimeY = 10;
+}
+
+function Update () {
+
+if(canSpawnRight == true)
+spawnEnemyRight();
+
+if(canSpawnLeft == true)
+spawnEnemyLeft();
+
+}
+
+
+function spawnEnemyLeft(){
+
+canSpawnLeft = false;
+Instantiate(llamaLeft, spawnRight.position, Quaternion.Euler(new Vector2(0,0)));
+
+yield WaitForSeconds(Random.Range(spawnTimeX,spawnTimeY));
+canSpawnLeft = true;
+}
+
+
+function spawnEnemyRight(){
+
+canSpawnRight = false;
+Instantiate(llamaRight, spawnLeft.position, Quaternion.Euler(new Vector2(0,0)));
+
+yield WaitForSeconds(Random.Range(spawnTimeX,spawnTimeY));
+canSpawnRight = true;
+}
